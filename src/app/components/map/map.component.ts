aecf9a071ff485887a65c510c20f9205913bdfbe
Artem Kabanin <akabanin6@gmail.com>
2023-01-28 17:27:25 +0200

    Add details popup

--- src/app/components/map/map.component.ts
+++ src/app/components/map/map.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input, ViewChild } from '@angular/core';
 import { GoogleMapsModule } from '@angular/google-maps';
 import { Observable, of } from 'rxjs';
 import { CommonModule } from '@angular/common';
@@ -7,6 +7,7 @@ import { HttpClientModule, HttpClientJsonpModule } from '@angular/common/http';
 import { MAP_API_KEY, MAP_API_URL } from 'src/app/providers/map.providers';
 import { GOOGLE_MAP_API_KEY, GOOGLE_MAP_API_URL } from 'src/app/configs/map-configs/map.providers';
 import { LatLngPipe } from 'src/app/pipes/lat-lng.pipe';
+import { MapInfoWindow, MapMarker } from '@angular/google-maps';
 import { PointLocation } from 'src/app/models/api.models';
 
 @Component({
@@ -33,9 +34,19 @@ export class MapComponent {
   @Input() center: google.maps.LatLngLiteral = { lat: 32, lng: 32 };
   @Input() zoom: number = 4;
 
+  @ViewChild(MapInfoWindow) infoWindow: MapInfoWindow | undefined;
+
 
   apiLoaded$: Observable<boolean> = this.service.load();
+  detailsContent: string = '';
 
   constructor(private service: MapService) {
+  }
+
+  showDetails(marker: MapMarker, details: string): void {
+    if (this.infoWindow) {
+      this.detailsContent = details;
+      this.infoWindow.open(marker);
+    }
   }
 }
